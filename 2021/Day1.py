80b26dbc343209fbedb7d3c7a2d860a3c25fb278
Jake Snow <jakesnow@btinternet.com>
2021-12-01 19:30:54 +0000

    Day 1 part 2

--- 2021/Day1.py
+++ 2021/Day1.py
@@ -1,12 +1,9 @@
 import os
 
-with open('Day1Input.txt', 'r') as file:
-	lines = file.read().split('\n')
-
+def count_increases(depths):
 	count = 0;
 	lastdepth = None
-	for line in lines:
-		depth = int(line)
+	for depth in depths:
 
 		if lastdepth == None:
 			lastdepth = depth
@@ -16,4 +13,21 @@ with open('Day1Input.txt', 'r') as file:
 			count = count + 1
 		lastdepth = depth
 
-	print("Day 1 solution: " + str(count))
+	return count
+
+with open('Day1Input.txt', 'r') as file:
+	lines = file.read().split('\n')
+
+	depths = [int(line) for line in lines]
+
+	print("Day 1 solution: " + str(count_increases(depths)))
+
+	windows = []
+	for i, depth in enumerate(depths):
+		if len(depths)-2 == i:
+			break
+
+		window = sum(depths[i:i+3])
+		windows.append(window)
+
+	print("Day 2 solution: " + str(count_increases(windows)))
