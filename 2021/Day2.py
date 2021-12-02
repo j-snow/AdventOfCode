d3e86beda845d440aad6e634dd11dc77ce32086a
j-snow <j-snow@users.noreply.github.com>
2021-12-02 20:58:14 +0000

    Day 2 - reduce duplication

--- 2021/Day2.py
+++ 2021/Day2.py
@@ -2,31 +2,8 @@ with open('Day2Input.txt') as file:
 	commands = file.readlines()
 
 	horizontal_pos = 0
-	depth = 0
-
-	for command in commands:
-		split = command.split(' ')
-		direction = split[0]
-		amount = int(split[1])
-
-		match direction:
-			case 'forward':
-				horizontal_pos = horizontal_pos + amount
-			case 'up':
-				depth = depth - amount
-			case 'down':
-				depth = depth + amount
-			case _:
-				raise ValueError('direction ' + direction + ' not reconised')
-
-	print("Horizontal position: " + str(horizontal_pos))
-	print("Depth: " + str(depth))
-	solution1 = horizontal_pos*depth
-	print("Day 2 part 1 solution: " + str(solution1))
-	assert solution1 == 1714680
-
-	horizontal_pos = 0
-	depth = 0
+	depth_part_1 = 0
+	depth_part_2 = 0
 	aim = 0
 
 	for command in commands:
@@ -37,16 +14,20 @@ with open('Day2Input.txt') as file:
 		match direction:
 			case 'forward':
 				horizontal_pos = horizontal_pos + amount
-				depth  = depth + (aim * amount)
+				depth_part_2  = depth_part_2 + (aim * amount)
 			case 'up':
+				depth_part_1 = depth_part_1 - amount
 				aim = aim - amount
 			case 'down':
+				depth_part_1 = depth_part_1 + amount
 				aim = aim + amount
 			case _:
-				raise ValueError('direction ' + direction + ' not reconised')
+				raise ValueError('direction ' + direction + ' not recognised')
 
-	print("Horizontal position: " + str(horizontal_pos))
-	print("Depth: " + str(depth))
-	solution2 = horizontal_pos*depth
-	print("Day 2 part 2 solution: " + str(solution2))
+	solution1 = horizontal_pos*depth_part_1
+	assert solution1 == 1714680
+	print("Day 2 part 1 solution: " + str(solution1))
+
+	solution2 = horizontal_pos*depth_part_2
 	assert solution2 == 1963088820
+	print("Day 2 part 2 solution: " + str(solution2))
