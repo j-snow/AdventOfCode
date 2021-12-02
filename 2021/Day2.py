d22dc60996c9eafccdeeb3451612f86cab2d87b7
j-snow <j-snow@users.noreply.github.com>
2021-12-02 21:02:35 +0000

    Day 2 - reduce duplication

--- 2021/Day2.py
+++ 2021/Day2.py
@@ -2,8 +2,7 @@ with open('Day2Input.txt') as file:
 	commands = file.readlines()
 
 	horizontal_pos = 0
-	depth_part_1 = 0
-	depth_part_2 = 0
+	depth = 0
 	aim = 0
 
 	for command in commands:
@@ -14,20 +13,18 @@ with open('Day2Input.txt') as file:
 		match direction:
 			case 'forward':
 				horizontal_pos = horizontal_pos + amount
-				depth_part_2  = depth_part_2 + (aim * amount)
+				depth  = depth + (aim * amount)
 			case 'up':
-				depth_part_1 = depth_part_1 - amount
 				aim = aim - amount
 			case 'down':
-				depth_part_1 = depth_part_1 + amount
 				aim = aim + amount
 			case _:
 				raise ValueError('direction ' + direction + ' not recognised')
 
-	solution1 = horizontal_pos*depth_part_1
+	solution1 = horizontal_pos*aim # aim is the old depth from part one
 	assert solution1 == 1714680
 	print("Day 2 part 1 solution: " + str(solution1))
 
-	solution2 = horizontal_pos*depth_part_2
+	solution2 = horizontal_pos*depth
 	assert solution2 == 1963088820
 	print("Day 2 part 2 solution: " + str(solution2))
