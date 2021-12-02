de6a358f57ffa8bba7e341dec82708e35baf7dbc
j-snow <j-snow@users.noreply.github.com>
2021-12-02 20:52:48 +0000

    Day 2, part 2

--- 2021/Day2.py
+++ 2021/Day2.py
@@ -1,8 +1,8 @@
 with open('Day2Input.txt') as file:
 	commands = file.readlines()
 
-	horizontal_pos = 0;
-	depth = 0;
+	horizontal_pos = 0
+	depth = 0
 
 	for command in commands:
 		split = command.split(' ')
@@ -21,6 +21,32 @@ with open('Day2Input.txt') as file:
 
 	print("Horizontal position: " + str(horizontal_pos))
 	print("Depth: " + str(depth))
-	solution = horizontal_pos*depth
-	print("Day 1 part 1 solution: " + str(solution))
-	assert solution == 1714680
+	solution1 = horizontal_pos*depth
+	print("Day 2 part 1 solution: " + str(solution1))
+	assert solution1 == 1714680
+
+	horizontal_pos = 0
+	depth = 0
+	aim = 0
+
+	for command in commands:
+		split = command.split(' ')
+		direction = split[0]
+		amount = int(split[1])
+
+		match direction:
+			case 'forward':
+				horizontal_pos = horizontal_pos + amount
+				depth  = depth + (aim * amount)
+			case 'up':
+				aim = aim - amount
+			case 'down':
+				aim = aim + amount
+			case _:
+				raise ValueError('direction ' + direction + ' not reconised')
+
+	print("Horizontal position: " + str(horizontal_pos))
+	print("Depth: " + str(depth))
+	solution2 = horizontal_pos*depth
+	print("Day 2 part 2 solution: " + str(solution2))
+	assert solution2 == 1963088820
