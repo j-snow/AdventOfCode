f16173627689ce81ac3880398c0719556be8accf
j-snow <j-snow@users.noreply.github.com>
2021-12-06 22:33:14 +0000

    Day 6, part 2

--- 2021/Day6.py
+++ 2021/Day6.py
@@ -1,34 +1,53 @@
+def prepare_array(array):
+	prepared_array = []
+	for x in range(0,9):
+		prepared_array.append(0)
+
+	for num in array:
+		prepared_array[num] = prepared_array[num] + 1
+
+	return prepared_array
+
 def add_fish(fish, amount):
-	#print(amount)
-	#print(fish)
-	new_input = []
-	new_fish = []
-
-	for num in fish:
-		if num == 0:
-			new_input.append(6)
-			new_fish.append(8)
-		else:
-			new_input.append(num-1)
-
-	new_input = new_input+new_fish
-
-	if amount == 1:
-		return new_input
+	if amount == 0:
+		return fish
 	else:
-		return add_fish(new_input, amount-1)
+		new = fish.pop(0)
+		fish[6] = fish[6] + new
+		fish.append(new)
+		
+		return add_fish(fish, amount-1)
 
+
+# Examples
 test_input = [3,4,3,1,2]
-test_expected = [6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8] #after 18 days
+prepared_test_input = prepare_array(test_input)
+assert prepared_test_input == [0,1,1,2,1,0,0,0,0]
 
-test_result = add_fish(test_input, 18)
+test_result = add_fish(prepared_test_input, 1)
 
-assert test_result == test_expected
-assert len(test_result) == 26
+prepared_test_input = prepare_array(test_input)
+test_result = add_fish(prepared_test_input, 18)
+assert sum(test_result) == 26
+assert test_result == prepare_array([6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8])
+
+prepared_test_input = prepare_array(test_input)
+test_result = add_fish(prepared_test_input, 80)
+assert sum(test_result) == 5934
 
 
+prepared_test_input = prepare_array(test_input)
+test_result = add_fish(prepared_test_input, 256)
+assert sum(test_result) == 26984457539
+
+
+# Solutions
 input = [1,1,1,1,1,5,1,1,1,5,1,1,3,1,5,1,4,1,5,1,2,5,1,1,1,1,3,1,4,5,1,1,2,1,1,1,2,4,3,2,1,1,2,1,5,4,4,1,4,1,1,1,4,1,3,1,1,1,2,1,1,1,1,1,1,1,5,4,4,2,4,5,2,1,5,3,1,3,3,1,1,5,4,1,1,3,5,1,1,1,4,4,2,4,1,1,4,1,1,2,1,1,1,2,1,5,2,5,1,1,1,4,1,2,1,1,1,2,2,1,3,1,4,4,1,1,3,1,4,1,1,1,2,5,5,1,4,1,4,4,1,4,1,2,4,1,1,4,1,3,4,4,1,1,5,3,1,1,5,1,3,4,2,1,3,1,3,1,1,1,1,1,1,1,1,1,4,5,1,1,1,1,3,1,1,5,1,1,4,1,1,3,1,1,5,2,1,4,4,1,4,1,2,1,1,1,1,2,1,4,1,1,2,5,1,4,4,1,1,1,4,1,1,1,5,3,1,4,1,4,1,1,3,5,3,5,5,5,1,5,1,1,1,1,1,1,1,1,2,3,3,3,3,4,2,1,1,4,5,3,1,1,5,5,1,1,2,1,4,1,3,5,1,1,1,5,2,2,1,4,2,1,1,4,1,3,1,1,1,3,1,5,1,5,1,1,4,1,2,1]
 
-solution = len(add_fish(input, 80))
+solution = sum(add_fish(prepare_array(input), 80))
 assert solution == 374994
 print("Day 6, part 1: " + str(solution))
+
+solution = sum(add_fish(prepare_array(input), 256))
+assert solution == 1686252324092
+print("Day 6, part 2: " + str(solution))
