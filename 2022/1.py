acbe3715b954486622ef9027cd80c80d43beef46
j-snow <j-snow@users.noreply.github.com>
2022-12-01 22:24:51 +0000

    Update Day 1 code for part 2

--- 2022/1.py
+++ 2022/1.py
@@ -1,4 +1,4 @@
-def get_highest_calories(file_name):
+def get_highest_calories(file_name, no_of_elves):
 	with open(file_name) as file:
 		lines = file.read().split('\n')
 
@@ -14,10 +14,16 @@ def get_highest_calories(file_name):
 		elves.append(elf)
 
 		elf_calorie_count = [sum(x) for x in elves]
+		elf_calorie_count.sort(reverse=True)
 
-		return max(elf_calorie_count)
+		top_elves = elf_calorie_count[:no_of_elves]
+		return sum(top_elves)
 
 
-assert get_highest_calories('1example.txt') == 24000
+assert get_highest_calories('1example.txt', 1) == 24000
 
-print(get_highest_calories('1input.txt'))
+print(get_highest_calories('1input.txt', 1))
+
+assert get_highest_calories('1example.txt', 3) == 45000
+
+print(get_highest_calories('1input.txt', 3))
