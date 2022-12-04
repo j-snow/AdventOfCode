d08f3eea62ea8f96d5f8b44628163acf89c686b4
j-snow <j-snow@users.noreply.github.com>
2022-12-04 22:52:43 +0000

    Day 3, part 2

--- 2022/3.py
+++ 2022/3.py
@@ -1,6 +1,21 @@
 import Util
 
 
+def array_intersect(arr1, arr2):
+	intersected_set = set(arr1) & set(arr2)
+	return list(intersected_set)
+
+
+def character_value(char):
+	ascii_no = ord(char)
+	if char.isupper():
+		char_value = ascii_no - 64 + 26
+	else:
+		char_value = ascii_no - 96
+
+	return char_value
+
+
 def count_character_values(file_name):
 	rucksacks = []
 	input = Util.split_file_by_new_line(file_name)
@@ -12,20 +27,37 @@ def count_character_values(file_name):
 
 	total = 0
 	for rucksack in rucksacks:
-		diff = list(set(rucksack[0]) & set(rucksack[1]))
+		diff = array_intersect(rucksack[0], rucksack[1])
 		char = diff[0]
-		char_no = None
-		ascii_no = ord(char)
-		if char.isupper():
-			char_no = ascii_no - 64 + 26
-		else:
-			char_no = ascii_no - 96
+		char_value = character_value(char)
 
-		total += char_no
+		total += char_value
 
 	return total
 
 
+def find_badges(file_name):
+	input = Util.split_file_by_new_line(file_name)
+	total = 0
+
+	while input != []:
+		three_rucksacks = input[:3]
+		input = input[3:]
+
+		common_character = array_intersect(array_intersect(three_rucksacks[0], three_rucksacks[1]), three_rucksacks[2])
+		assert len(common_character) == 1
+		char_value = character_value(common_character[0])
+		total += char_value
+
+	return total
+
+
+# Part 1
 assert count_character_values('3example.txt') == 157
 assert count_character_values('3input.txt') == 7766
 print(count_character_values('3input.txt'))
+
+# Part 2
+assert find_badges('3example.txt') == 70
+assert find_badges('3input.txt') == 2415
+print(find_badges('3input.txt'))
