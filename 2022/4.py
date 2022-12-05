32af12b459ca64ac55e2adb1baac73e1b81206a9
j-snow <j-snow@users.noreply.github.com>
2022-12-05 21:58:41 +0000

    Remove print()

--- 2022/4.py
+++ 2022/4.py
@@ -20,12 +20,10 @@ def find_overlap_count(file_name):
 		second = (int(second[0]), int(second[1]))
 
 		if first[0] <= second[0] and first[1] >= second[1]:
-			print([first, second])
 			total += 1
 			continue
 
 		if second[0] <= first[0] and second[1] >= first[1]:
-			print([first, second])
 			total += 1
 			continue
 
