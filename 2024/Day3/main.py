bddd24a3f8924b43e65cbbba4b8ab29addeba10f
j-snow <j-snow@users.noreply.github.com>
2024-12-04 00:10:19 +0000

    2024 day 3, part 2 attempt

--- 2024/Day3/main.py
+++ 2024/Day3/main.py
@@ -1,44 +1,30 @@
 import re
+from pprint import pprint
 
 
-def parse_input(file):
+def parse_input_part1(file):
 	multiplications = []
 	for line in file:
 		multiplications.extend(re.findall('mul\((\d{1,3}),(\d{1,3})\)', line))
 
 	return multiplications
 
-def is_safe(report):
-	safe = True
-	contains_positive = False
-	contains_negative = False
-	diffs = [report[i] - report[i + 1] for i in range(0, len(report) - 1)]
-	for number in diffs:
-		if number == 0:
-			safe = False
-			continue
-		if abs(number) > 3:
-			safe = False
-			continue
-		if number < 0:
-			contains_negative = True
-		if number > 0:
-			contains_positive = True
-		if contains_positive and contains_negative:
-			safe = False
-			continue
 
-	return safe
+def parse_input_part2(file):
+	multiplications = []
+	for line in file:
+		dos = []
+		parts = line.split('do()')
+		for part in parts:
+			dos.append(part.split("don't()")[0])
 
-def part_1(multiplications):
-	total = 0
+		for do in dos:
+			multiplications.extend(re.findall('mul\((\d{1,3}),(\d{1,3})\)', do))
 
-	for multiplication in multiplications:
-		total += int(multiplication[0]) * int(multiplication[1])
+	return multiplications
 
-	return total
 
-def part_2(reports):
+def multiply(multiplications):
 	total = 0
 
 	for multiplication in multiplications:
